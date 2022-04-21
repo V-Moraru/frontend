242d2892f6206242c2f5b4c9dca892bfb698cce2
Valentyn Moraru <moraruvalentyn@gmail.com>
2022-04-21 08:50:42 +0300

    rename theme: fix 2 files

--- Magento/Drexel/registration.php
+++ Magento/Drexel/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/skeleton', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/Drexel', __DIR__);
