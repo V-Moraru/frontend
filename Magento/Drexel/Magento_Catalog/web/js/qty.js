5aa599447768a1760c583f52b8ddfd7bbfcd4900
Valentyn Moraru <moraruvalentyn@gmail.com>
2022-05-31 14:10:31 +0300

    Edit Qty widget

--- Magento/Drexel/Magento_Catalog/web/js/qty.js
+++ Magento/Drexel/Magento_Catalog/web/js/qty.js
@@ -8,24 +8,30 @@ define([
             $qtyDown = $(this.element).find(".qty-down");
             $qtyUp = $(this.element).find(".qty-up");
             $qtyInput = $(this.element).find("input.qty");
+            $defaultValue = 1;
 
             $qtyUp.click(function(e) {
                 e.preventDefault();
                 $value = parseInt($qtyInput.val());
-                $value++;
-                $qtyInput.val($value);
+                if (!isNaN($value)) {
+                    $value++;
+                    $qtyInput.val($value);
+                } else {
+                    $qtyInput.val($defaultValue);
+                }
             });
 
             $qtyDown.click(function(e) {
                 e.preventDefault();
                 $value = parseInt($qtyInput.val());
-                if ($value > 1) {
+                if (!isNaN($value) && $value > 1) {
                     $value--;
                     $qtyInput.val($value);
+                } else {
+                    $qtyInput.val($defaultValue);
                 }
             });
         },
-
 });
 
 return $.mage.qty;
